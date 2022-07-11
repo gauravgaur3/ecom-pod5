de9e225b9d2aecf708c8857f3977510a47992990
Tanushree Sharma <tanushreesharma544@gmail.com>
2022-07-11 17:56:31 +0530

    images

--- src/components/TopSlider.jsx
+++ src/components/TopSlider.jsx
@@ -3,24 +3,24 @@ import Carousel from 'react-bootstrap/Carousel';
 function TopSlider() {
   return (
     <Carousel variant="dark">
-      <Carousel.Item style={{'height':"300px"}}>
+      <Carousel.Item style={{'height':"600px"}}>
         <img
           className="d-block w-100"
-          src={'assets/header5.jpg'}
+          src={'assets/header1.jpg'}
           alt="First slide"
         />
       </Carousel.Item>
-      <Carousel.Item style={{'height':"300px"}}>
+      <Carousel.Item style={{'height':"600px"}}>
         <img
           className="d-block w-100"
-          src={'assets/header3.jpg'}
+          src={'assets/header2.jpg'}
           alt="Second slide"
         />
       </Carousel.Item>
-      <Carousel.Item style={{'height':"300px"}}>
+      <Carousel.Item style={{'height':"600px"}}>
         <img
           className="d-block w-100"
-          src={'assets/header4.jpg'}
+          src={'assets/header3.jpg'}
           alt="Third slide"
         />
       </Carousel.Item>
